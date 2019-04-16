006d3d314891b76ac7969247bcfad04a73e8de5a
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-16 04:35:27 -0400

    app.use for the routes has been added

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ const {getRatingsRouter, } = require('./routes/ratings')
 
 const getApp = () => {
     const app = express();
+    app.use(bodyParser.json())
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin' , "*");
+        next();
+    });
+
+    app.use('/comments', getCommentsRouter());
+    app.use('/genres', getGenresRouter());
+    app.use('/movies', getMoviesRouter());
+    app.use('/ratings', getRatingsRouter());
+
+    return app;
+
+}
