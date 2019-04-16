9bb6ca25b8ca74d25783cc2278dfbb346c2a00fd
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-16 03:18:12 -0400

    movies added, starting ratings

--- seed.sql
+++ seed.sql
@@ -46,8 +46,8 @@ CREATE TABLE comments (
 + each movie have a specific id number refence*/
 
 /*genres*/
-INSERT INTO genres (name) VALUES ('comedy');
-INSERT INTO genres (name) VALUES ('romance');
+INSERT INTO genres (name) VALUES ('comedy'); 
+INSERT INTO genres (name) VALUES ('romance'); 
 INSERT INTO genres (name) VALUES ('drama');
 INSERT INTO genres (name) VALUES ('action');
 INSERT INTO genres (name) VALUES ('horror');
@@ -67,7 +67,7 @@ INSERT INTO movies (title, genre_id)
 VALUES ('While We Are Young', 2, 'https://i.pinimg.com/originals/91/d8/32/91d83213d3a7192fc12cdf94c0b9383b.jpg');
 
 INSERT INTO movies (title, genre_id)
-VALUES ('Funny Face', 2, 'https://images-na.ssl-images-amazon.com/images/I/51Wc8fB1daL.jpg');
+VALUES ('Second Act', 2, 'https://popcornusa.s3.amazonaws.com/movies/650/11003-0-SecondAcjpg');
 
 INSERT INTO movies (title, genre_id)
 VALUES ('The Spectacular Now', 2, 'https://m.media-amazon.com/images/M/MV5BMjA5MTc0NTkzM15BMl5BanBnXkFtZTcwODEwNjE3OQ@@._V1_.jpg');
@@ -76,4 +76,24 @@ INSERT INTO movie (title, genre_id, img_url)
 VALUES ('Moonlight', 3, 'https://img.moviepostershop.com/moonlight-movie-poster-2016-1020776692.jpg');
 
 INSERT INTO movie (title, genre_id, img_url)
+VALUES ('Glass',3,'https://m.media-amazon.com/images/M/MV5BMTY1OTA2MjI5OV5BMl5BanBnXkFtZTgwNzkxMjU4NjM@._V1_UY268_CR3,0,182,268_AL_.jpg');
+
+INSERT INTO movie (title, genre_id, img_url)
+VALUES ('Room',3,'http://www.creativehomex.com/wp-content/uploads/2016/06/room-movie-poster-web.jpg');
+
+INSERT INTO movie (title, genre_id, img_url)
+VALUES ('Polar',4,'https://m.media-amazon.com/images/M/MV5BMjAzNDkzODU3Ml5BMl5BanBnXkFtZTgwNDI4OTExNzM@._V1_UX182_CR0,0,182,268_AL_.jpg');
+
+INSERT INTO movie (title, genre_id, img_url)
+VALUES ('Lila & Eve',4,'');
+
+INSERT INTO movie (title, genre_id, img_url)
 VALUES ( 'Slice', 5, 'https://m.media-amazon.com/images/M/MV5BMTk1MTA0NzAwNF5BMl5BanBnXkFtZTgwNDE2MzEzNjM@._V1_UY1200_CR90,0,630,1200_AL_.jpg');
+
+INSERT INTO movie (title, genre_id, img_url)
+VALUES ( 'US', 5, 'https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg');
+
+INSERT INTO movie (title, genre_id, img_url)
+VALUES ('JURASSIC WORLD: FALLEN KINGDOM',6,'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6257/6257211_sa.jpg;maxHeight=640;maxWidth=550');
+
+/*ratings*/
