e7e1f1a9f9bba472ffd20192d13355151a9f505a
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-04-16 04:22:56 -0400

    routes added to app

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {getCommentsRouter, } = require('./routes/comments')
+const {getGenresRouter, } = require('./routes/genres')
+const {getMoviesRouter, } = require('./routes/movies')
+const {getRatingsRouter, } = require('./routes/ratings')
